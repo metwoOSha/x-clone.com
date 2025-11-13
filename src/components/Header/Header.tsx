91354edcf404b952b2c22c638d23a4cb2f4f0dec
Dmytro Dobrovolskyi <dimadobrovolski@gmail.com>
2025-11-14 00:23:20 +0200

    icons

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,55 @@
+import Image from 'next/image';
+
 import cls from './Header.module.css';
 
 export function Header() {
-	return (
-		<div className={cls.flex}>
-			<div>
-				
-			</div>
-			<div></div>
-		</div>
-	)
+    return (
+        <div className={cls.flex}>
+            <div>
+                <Image src="./x-com.svg" alt="x-icon" width={24} height={24} />
+            </div>
+            <nav className={cls.navFlex}>
+                <div className={cls.navBlock}>
+                    <Image src="./x-com.svg" alt="x-icon" />
+                    <span className={cls.navText}>Home</span>
+                </div>
+                <div className={cls.navBlock}>
+                    <Image src="./x-com.svg" alt="x-icon" />
+                    <span className={cls.navText}>Explore</span>
+                </div>
+                <div className={cls.navBlock}>
+                    <Image src="./x-com.svg" alt="x-icon" />
+                    <span className={cls.navText}>Notifications</span>
+                </div>
+                <div className={cls.navBlock}>
+                    <Image src="./x-com.svg" alt="x-icon" />
+                    <span className={cls.navText}>Messages</span>
+                </div>
+                <div className={cls.navBlock}>
+                    <Image src="./x-com.svg" alt="x-icon" />
+                    <span className={cls.navText}>Lists</span>
+                </div>
+                <div className={cls.navBlock}>
+                    <Image src="./x-com.svg" alt="x-icon" />
+                    <span className={cls.navText}>Bookmarks</span>
+                </div>
+                <div className={cls.navBlock}>
+                    <Image src="./x-com.svg" alt="x-icon" />
+                    <span className={cls.navText}>Communities</span>
+                </div>
+                <div className={cls.navBlock}>
+                    <Image src="./x-com.svg" alt="x-icon" />
+                    <span className={cls.navText}>Verified Orgs</span>
+                </div>
+                <div className={cls.navBlock}>
+                    <Image src="./x-com.svg" alt="x-icon" />
+                    <span className={cls.navText}>Profile</span>
+                </div>
+                <div className={cls.navBlock}>
+                    <Image src="./x-com.svg" alt="x-icon" />
+                    <span className={cls.navText}>More</span>
+                </div>
+            </nav>
+        </div>
+    );
 }
